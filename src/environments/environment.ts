58fa3188631f4dc5c4475d2f43930dd15e4a67c1
smidori <smidori@gmail.com>
2023-08-12 20:36:40 +0100

    environment settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: "http://34.159.242.97:8181/api"
-    //apiURL: "http://localhost:8181/api"
-    // apiURL: "http://bs-java:8181/api"
+    apiURL: "http://bs-java:8080/api"
 };
